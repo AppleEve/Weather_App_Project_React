b8d97a62d26c5e37241e88b5de1027c35001a8ca
AppleEve <etandruszkiewicz@gmail.com>
2022-08-25 16:29:02 +0300

    Creating conditional rendering changing background

--- src/App.js
+++ src/App.js
@@ -6,27 +6,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="current-weather">
-        <SearchEngine defaultCity="Nicosia" />
-        <footer>
-          The app was coded by{" "}
-          <a
-            href="https://tubular-starlight-a3612b.netlify.app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            AppleEve{" "}
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/AppleEve/Weather_App_Project_React"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced.
-          </a>
-        </footer>
-      </div>
+      <SearchEngine defaultCity="Nicosia" />
+      <footer>
+        The app was coded by{" "}
+        <a
+          href="https://tubular-starlight-a3612b.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          AppleEve{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AppleEve/Weather_App_Project_React"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced.
+        </a>
+      </footer>
     </div>
   );
 }
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./SearchEngine.css";
-
+//import BackgroundImage from "./BackgroundImage";
 import WeatherInfo from "./WeatherInfo";
+// Background images
+import Sunny from "./img/01d.png";
+import CleanNight from "./img/02n.png";
+import PartlyCloudyDay from "./img/02d.png";
+import PartlyCloudyNight from "./img/02n.png";
+import CloudyDay from "./img/03d.png";
+import CloudyNight from "./img/03n.png";
+import RainyDay from "./img/09d.png";
+import RainyNight from "./img/09n.png";
+import StormDay from "./img/11d.png";
+import StormNight from "./img/11n.png";
+import SnowDay from "./img/13d.png";
+import SnowNight from "./img/13n.png";
+import FogDay from "./img/50d.png";
+import FogNight from "./img/50n.png";
 
 export default function SearchEngine(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -13,6 +28,7 @@ export default function SearchEngine(props) {
       temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
       wind: Math.round(response.data.wind.speed),
       city: response.data.name,
       country: response.data.sys.country,
@@ -38,16 +54,61 @@ export default function SearchEngine(props) {
 
   if (weatherData.ready) {
     return (
-      <div className="SearchEngine row">
-        <form onSubmit={handleSubmit}>
-          <input
-            onChange={updateCity}
-            className="search-window col-9"
-            type="search"
-            placeholder="select a city..."
-          />
-          <input className="submit-button col-2" type="submit" value="search" />
-        </form>
+      <div
+        className="current-weather"
+        style={{
+          backgroundImage:
+            weatherData.icon === "01d"
+              ? `url(${Sunny})`
+              : weatherData.icon === "01n"
+              ? `url(${CleanNight})`
+              : weatherData.icon === "02d"
+              ? `url(${PartlyCloudyDay})`
+              : weatherData.icon === "02n"
+              ? `url(${PartlyCloudyNight})`
+              : weatherData.icon === "03d" || weatherData.icon === "04d"
+              ? `url(${CloudyDay})`
+              : weatherData.icon === "03n" || weatherData.icon === "04n"
+              ? `url(${CloudyNight})`
+              : weatherData.icon === "09d" || weatherData.icon === "10d"
+              ? `url(${RainyDay})`
+              : weatherData.icon === "09n" || weatherData.icon === "10n"
+              ? `url(${RainyNight})`
+              : weatherData.icon === "11d"
+              ? `url(${StormDay})`
+              : weatherData.icon === "11n"
+              ? `url(${StormNight})`
+              : weatherData.icon === "13d"
+              ? `url(${SnowDay})`
+              : weatherData.icon === "13n"
+              ? `url(${SnowNight})`
+              : weatherData.icon === "50d"
+              ? `url(${FogDay})`
+              : weatherData.icon === "50n"
+              ? `url(${FogNight})`
+              : `url(${CloudyDay})`,
+          backgroundSize: "cover",
+          width: "100%",
+          padding: "30px",
+          borderRadius: "17px",
+          transition: "2s",
+        }}
+      >
+        <div className="SearchEngine row">
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={updateCity}
+              className="search-window col-9"
+              type="search"
+              placeholder="select a city..."
+            />
+            <input
+              className="submit-button col-2"
+              type="submit"
+              value="search"
+            />
+          </form>
+        </div>
         <WeatherInfo data={weatherData} />
       </div>
     );

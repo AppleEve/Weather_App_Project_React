97e88e81a455e4aead3bf96151dce57fc7f3a55d
AppleEve <etandruszkiewicz@gmail.com>
2022-08-28 13:57:38 +0300

    Connecting API with the layout

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,40 +1,36 @@
-import React from "react";
-import WeatherIcons from "./WeatherIcons";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  function updateWeatherForecast(response) {
-    console.log(response.data);
-  }
-  let lon = props.data.lon;
-  let lat = props.data.lat;
-  let keyAPI = "711bf416fd4b68649d4f2e89cc233151";
-  let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${keyAPI}&units=metric`;
-  axios.get(url).then(updateWeatherForecast);
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  return (
-    <div className="WeatherForecast row">
-      <div className="col">
-        <div className="day">tuesday</div>
-        <WeatherIcons code="01d" color="#7996d1" />
-        <div className="temperature">20°C </div>
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
       </div>
-      <div className="col">
-        <div className="day">tuesday</div>
-        <WeatherIcons code="02d" color="#7996d1" />
-        <div className="temperature">20°C </div>
-      </div>
-      <div className="col">
-        <div className="day">tuesday</div>
-        <WeatherIcons code="11d" color="#7996d1" />
-        <div className="temperature">20°C </div>
-      </div>
-      <div className="col">
-        <div className="day">tuesday</div>
-        <WeatherIcons code="50d" color="#7996d1" />
-        <div className="temperature">20°C </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "711bf416fd4b68649d4f2e89cc233151";
+    let longitude = props.data.lon;
+    let latitude = props.data.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
